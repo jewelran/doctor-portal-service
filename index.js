875b40ac8d2173f24e33199f3d1a6b2880c7e3b7
jewelran <mdjewelrana6487@gmail.com>
2021-06-16 21:27:53 +0600

    add appointment

--- index.js
+++ index.js
@@ -1,32 +1,41 @@
-const express = require('express')
-require('dotenv').config()
-const cors = require('cors')
-const  bodyParser = require('body-parser')
-const app = express()
-app.use(cors())
-app.use(bodyParser.json())
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
 
+const port = process.env.PORT || 5000;
 
-const port =process.env.PORT || 5000
-
+console.log(process.env.DB_USER,process.env.DB_PASS);
 
 
 const MongoClient = require('mongodb').MongoClient;
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dqp4e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cb95t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-  const collection = client.db("test").collection("devices");
- 
+  const appointmentCollection = client.db("storeAppointment").collection("storeAppointmentInfo");
+  app.post("/addAppointment",(req, res) => {
+    const appointment = req.body;
+    appointmentCollection.insertOne(appointment)
+    .then(response => {
+      res.send(response.insertedCount > 0)
+    })
+  });
+   
+  app.post("/appointmentByDate", (req, res) =>{
+    const date = req.body;
+    appointmentCollection.find({date: date.data})
+    .toArray((err, documents) => {
+      res.send(documents)
+    })
+  })
+  
 });
 
+app.get("/", (req, res) => {
+  res.send("Hello World! i am here");
+});
 
-
-
-
-
-
-app.get('/', (req, res) => {
-  res.send('Hello World! i am here')
-})
-
-app.listen (process.env.PORT || port)
+app.listen(process.env.PORT || port);
